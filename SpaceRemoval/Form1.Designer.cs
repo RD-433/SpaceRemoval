29a2b40f6d331917bad568d6b20f365371867dea
RD-433 <73433021+RD-433@users.noreply.github.com>
2025-10-18 19:28:17 +0400

    Add files via upload

---
+++ SpaceRemoval/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace SpaceRemoval
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxIn = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOut = new System.Windows.Forms.RichTextBox();
+            this.buttonPaste = new System.Windows.Forms.Button();
+            this.buttonPCC = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.checkBoxRemoveIllegal = new System.Windows.Forms.CheckBox();
+            this.checkBoxContPar = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxDot = new System.Windows.Forms.CheckBox();
+            this.checkBoxComma = new System.Windows.Forms.CheckBox();
+            this.checkBoxColon = new System.Windows.Forms.CheckBox();
+            this.checkBoxSemicolon = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBoxIn
+            // 
+            this.richTextBoxIn.Location = new System.Drawing.Point(12, 141);
+            this.richTextBoxIn.Name = "richTextBoxIn";
+            this.richTextBoxIn.Size = new System.Drawing.Size(683, 407);
+            this.richTextBoxIn.TabIndex = 0;
+            this.richTextBoxIn.Text = "";
+            // 
+            // richTextBoxOut
+            // 
+            this.richTextBoxOut.Location = new System.Drawing.Point(725, 107);
+            this.richTextBoxOut.Name = "richTextBoxOut";
+            this.richTextBoxOut.ReadOnly = true;
+            this.richTextBoxOut.Size = new System.Drawing.Size(683, 441);
+            this.richTextBoxOut.TabIndex = 1;
+            this.richTextBoxOut.Text = "";
+            // 
+            // buttonPaste
+            // 
+            this.buttonPaste.Location = new System.Drawing.Point(568, 12);
+            this.buttonPaste.Name = "buttonPaste";
+            this.buttonPaste.Size = new System.Drawing.Size(75, 48);
+            this.buttonPaste.TabIndex = 2;
+            this.buttonPaste.Text = "Paste";
+            this.buttonPaste.UseVisualStyleBackColor = true;
+            this.buttonPaste.Click += new System.EventHandler(this.buttonPaste_Click);
+            // 
+            // buttonPCC
+            // 
+            this.buttonPCC.Location = new System.Drawing.Point(1333, 12);
+            this.buttonPCC.Name = "buttonPCC";
+            this.buttonPCC.Size = new System.Drawing.Size(75, 62);
+            this.buttonPCC.TabIndex = 3;
+            this.buttonPCC.Text = "Paste Convert Copy";
+            this.buttonPCC.UseVisualStyleBackColor = true;
+            this.buttonPCC.Click += new System.EventHandler(this.buttonPCC_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(786, 12);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 48);
+            this.buttonCopy.TabIndex = 4;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(675, 12);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 48);
+            this.buttonConvert.TabIndex = 5;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // checkBoxRemoveIllegal
+            // 
+            this.checkBoxRemoveIllegal.AutoSize = true;
+            this.checkBoxRemoveIllegal.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxRemoveIllegal.Name = "checkBoxRemoveIllegal";
+            this.checkBoxRemoveIllegal.Size = new System.Drawing.Size(126, 17);
+            this.checkBoxRemoveIllegal.TabIndex = 6;
+            this.checkBoxRemoveIllegal.Text = "Remove Illegal Chars";
+            this.checkBoxRemoveIllegal.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxContPar
+            // 
+            this.checkBoxContPar.AutoSize = true;
+            this.checkBoxContPar.Location = new System.Drawing.Point(6, 19);
+            this.checkBoxContPar.Name = "checkBoxContPar";
+            this.checkBoxContPar.Size = new System.Drawing.Size(120, 17);
+            this.checkBoxContPar.TabIndex = 7;
+            this.checkBoxContPar.Text = "Continue Paragraph";
+            this.checkBoxContPar.UseVisualStyleBackColor = true;
+            this.checkBoxContPar.CheckedChanged += new System.EventHandler(this.checkBoxContPar_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxSemicolon);
+            this.groupBox1.Controls.Add(this.checkBoxColon);
+            this.groupBox1.Controls.Add(this.checkBoxComma);
+            this.groupBox1.Controls.Add(this.checkBoxDot);
+            this.groupBox1.Controls.Add(this.checkBoxContPar);
+            this.groupBox1.Location = new System.Drawing.Point(207, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(242, 123);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // checkBoxDot
+            // 
+            this.checkBoxDot.AutoSize = true;
+            this.checkBoxDot.Location = new System.Drawing.Point(26, 42);
+            this.checkBoxDot.Name = "checkBoxDot";
+            this.checkBoxDot.Size = new System.Drawing.Size(53, 17);
+            this.checkBoxDot.TabIndex = 8;
+            this.checkBoxDot.Text = ". (dot)";
+            this.checkBoxDot.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxComma
+            // 
+            this.checkBoxComma.AutoSize = true;
+            this.checkBoxComma.Location = new System.Drawing.Point(85, 42);
+            this.checkBoxComma.Name = "checkBoxComma";
+            this.checkBoxComma.Size = new System.Drawing.Size(72, 17);
+            this.checkBoxComma.TabIndex = 9;
+            this.checkBoxComma.Text = ", (comma)";
+            this.checkBoxComma.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxColon
+            // 
+            this.checkBoxColon.AutoSize = true;
+            this.checkBoxColon.Location = new System.Drawing.Point(163, 42);
+            this.checkBoxColon.Name = "checkBoxColon";
+            this.checkBoxColon.Size = new System.Drawing.Size(64, 17);
+            this.checkBoxColon.TabIndex = 10;
+            this.checkBoxColon.Text = ": (colon)";
+            this.checkBoxColon.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSemicolon
+            // 
+            this.checkBoxSemicolon.AutoSize = true;
+            this.checkBoxSemicolon.Location = new System.Drawing.Point(26, 65);
+            this.checkBoxSemicolon.Name = "checkBoxSemicolon";
+            this.checkBoxSemicolon.Size = new System.Drawing.Size(85, 17);
+            this.checkBoxSemicolon.TabIndex = 11;
+            this.checkBoxSemicolon.Text = "; (semicolon)";
+            this.checkBoxSemicolon.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1429, 560);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBoxRemoveIllegal);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonPCC);
+            this.Controls.Add(this.buttonPaste);
+            this.Controls.Add(this.richTextBoxOut);
+            this.Controls.Add(this.richTextBoxIn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxIn;
+        private System.Windows.Forms.RichTextBox richTextBoxOut;
+        private System.Windows.Forms.Button buttonPaste;
+        private System.Windows.Forms.Button buttonPCC;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.CheckBox checkBoxRemoveIllegal;
+        private System.Windows.Forms.CheckBox checkBoxContPar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxColon;
+        private System.Windows.Forms.CheckBox checkBoxComma;
+        private System.Windows.Forms.CheckBox checkBoxDot;
+        private System.Windows.Forms.CheckBox checkBoxSemicolon;
+    }
+}
+
---
+++ SpaceRemoval/Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpaceRemoval.Properties;
+
+namespace SpaceRemoval
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> LegalChars = new List<string>()
+        {
+            "ა", "ბ", "გ", "დ", "ე", "ვ", "ზ",
+            "თ", "ი", "კ", "ლ", "მ", "ნ", "ო",
+            "პ", "ჟ", "რ", "ს", "ტ", "უ", "ფ",
+            "ქ", "ღ", "ყ", "შ", "ჩ", "ც", "ძ",
+            "წ", "ჭ", "ხ", "ჯ", "ჰ",
+
+            "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U",
+            "V", "W", "X", "Y", "Z",
+
+            "a", "b", "c", "d", "e", "f", "g",
+            "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z",
+
+            "А", "Б", "В", "Г", "Д", "Е", "Ё",
+            "Ж", "З", "И", "Й", "К", "Л", "М",
+            "Н", "О", "П", "Р", "С", "Т", "У",
+            "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ",
+            "Ы", "Ь", "Э", "Ю", "Я",
+
+            "а", "б", "в", "г", "д", "е", "ё",
+            "ж", "з", "и", "й", "к", "л", "м",
+            "н", "о", "п", "р", "с", "т", "у",
+            "ф", "х", "ц", "ч", "ш", "щ", "ъ",
+            "ы", "ь", "э", "ю", "я",
+
+            "0", "1", "2", "3", "4", "5", "6",
+            "7", "8", "9",
+
+            "\r", "\n","\t",
+
+            " ", "!", "\"", "#", "$", "%", "&", "\'", "(",
+            ")", "*", "+", ",", "-",".", "/",
+
+            ":", ";", "<", "=", ">", "?", "@",
+
+            "[", "\\", "]", "^", "_", "`",
+
+            "{", "|", "}", "~",
+
+            "’", ",", "“", "„", "”"
+
+        };
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            buttonCopy.Text = "Copy as Unicode";
+            buttonPaste.Text = "Paste as Unicode";
+
+            //this.Icon = Resources.tsu_logo;
+            this.Icon = Resources.autorun;
+            this.Text = "Space Removal v1.0.3";
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1_SizeChanged(sender, e);
+            checkBoxContPar_CheckedChanged(sender, e);
+            checkBoxDot.Checked = true;
+            checkBoxSemicolon.Checked = true;
+        }
+
+        private void Convert()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string endString = "";
+            int badCharCount = 0;
+
+            //string[] splitted = richTextBoxIn.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] splitted = richTextBoxIn.Text.Split(new char[] { ' ' }, StringSplitOptions.None);
+
+            foreach (string s in splitted)
+            {
+                //sb.Append(s.Trim());
+                bool badChar = false;
+                if (checkBoxRemoveIllegal.Checked)
+                {
+                    foreach (char s1 in s)
+                    {
+                        if (!LegalChars.Contains(s1.ToString()))
+                        {
+                            badChar = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (badChar)
+                {
+                    // rebuild string s = replace illegal chars with ""
+                    StringBuilder sb1 = new StringBuilder();
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        if (LegalChars.Contains(s[i].ToString()))
+                        {
+                            sb1.Append(s[i]);
+                        }
+                        else
+                        {
+                            // ""
+                            badCharCount++;
+                        }
+                    }
+                    sb1.Append(' ');
+                    sb.Append(sb1.ToString());
+
+                }
+                else
+                {
+                    sb.Append(s);
+                    sb.Append(' ');
+                }
+
+                if (sb.Length > 60000)
+                {
+                    endString += sb.ToString();
+                    sb.Clear();
+                }
+            }
+            endString += sb.ToString();
+            //richTextBoxOut.Text = sb.ToString();
+
+
+            int continuedPar = 0;
+
+            if (checkBoxContPar.Checked)
+            {
+                splitted = endString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                sb.Clear();
+                endString = "";
+
+                List<char> endChars = new List<char>();
+
+                if (checkBoxDot.Checked)
+                    endChars.Add('.');
+
+                if (checkBoxComma.Checked)
+                    endChars.Add(',');
+
+                if (checkBoxColon.Checked)
+                    endChars.Add(':');
+
+                if (checkBoxSemicolon.Checked)
+                    endChars.Add(';');
+
+                if (endChars.Count < 1)
+                {
+                    MessageBox.Show("Selecte at least 1 (one) char!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                //bool skipNext = false;
+
+                //foreach (string s in splitted)
+                for (int i = 0; i < splitted.Length; i++)
+                {
+
+                    if (splitted[i].EndsWith("\n") || splitted[i].EndsWith("\r"))
+                    {
+                        if (splitted[i].Equals("\n") || splitted[i].Equals("\r"))
+                        {
+                            sb.Append(splitted[i]);
+                            sb.Append(' ');
+                            continue;
+                        }
+
+
+                        if (endChars.Contains(splitted[i][splitted[i].Length - 2]))
+                        {
+                            // end of sentence, new paragraph allowed
+                            sb.Append(splitted[i]);
+                            sb.Append(' ');
+                        }
+                        else
+                        {
+                            // end of paragraph, but no end of sentence ! Remove next CRLF !
+                            //if (splitted[i + 1].Equals("\n") || splitted[i + 1].Equals("\r"))
+                            //{
+                            //    //skipNext = true;
+                            //}
+                            sb.Append(splitted[i].Substring(0, splitted[i].Length - 1));
+                            sb.Append(' ');
+                            continuedPar++;
+                        }
+                    }
+                    //else if (splitted[i].EndsWith("\r\n"))
+                    //{
+                    //    if (splitted[i].Equals("\r\n"))
+                    //    {
+                    //        sb.Append(splitted[i]);
+                    //        sb.Append(' ');
+                    //        continue;
+                    //    }
+
+                    //    if (endChars.Contains(splitted[i][splitted[i].Length - 3]))
+                    //    {
+                    //        sb.Append(splitted[i]);
+                    //        sb.Append(' ');
+                    //    }
+                    //    else
+                    //    {
+                    //        //if (splitted[i + 1].Equals("\r\n"))
+                    //        //{
+                    //        //    //skipNext = true;
+                    //        //}
+                    //        sb.Append(splitted[i].Substring(0, splitted[i].Length - 2));
+                    //        sb.Append(' ');
+                    //        continuedPar++;
+                    //    }
+                    //}
+                    else
+                    {
+                        if (i == 30)
+                        {
+                            i = i;
+                        }
+                        if (splitted[i].Contains("\n"))
+                        {
+                            var splitted2 = splitted[i].Split('\n');
+
+                            //foreach (var s in splitted2)
+                            //{
+                            //    if (s.Length <= 1)
+                            //    {
+                            //        sb.Append('\n');
+                            //        continue;
+                            //    }
+                            //    if (endChars.Contains(s[s.Length - 1]))
+                            //    {
+                            //        sb.Append(s);
+                            //        sb.Append('\n');
+                            //    }
+                            //    else
+                            //    {
+                            //        sb.Append(s);
+                            //        sb.Append(' ');
+                            //        continuedPar++;
+                            //    }
+                            //}
+
+                            //foreach (var s in splitted2)
+                            for (int j = 0; j < splitted2.Length - 1; j++)
+                            {
+                                if (splitted2[j].Length <= 1)
+                                {
+                                    sb.Append('\n');
+                                    continue;
+                                }
+                                if (endChars.Contains(splitted2[j][splitted2[j].Length - 1]))
+                                {
+                                    sb.Append(splitted2[j]);
+                                    sb.Append('\n');
+                                }
+                                else
+                                {
+                                    sb.Append(splitted2[j]);
+                                    sb.Append(' ');
+                                    continuedPar++;
+                                }
+                            }
+                            sb.Append(splitted2[splitted2.Length - 1]);
+                            sb.Append(' ');
+                        }
+                        //else if (splitted[i].Contains("\r"))
+                        //{
+                        //    var splitted2 = splitted[i].Split('\r');
+
+                        //    foreach (var s in splitted2)
+                        //    {
+                        //        if (s.Length <= 1)
+                        //        {
+                        //            sb.Append('\r');
+                        //            continue;
+                        //        }
+                        //        if (endChars.Contains(s[s.Length - 1]))
+                        //        {
+                        //            sb.Append(s);
+                        //            sb.Append('\r');
+                        //        }
+                        //        else
+                        //        {
+                        //            sb.Append(s);
+                        //            sb.Append(' ');
+                        //            continuedPar++;
+                        //        }
+                        //    }
+                        //}
+                        //else if (splitted[i].Contains("\r\n"))
+                        //{
+                        //    var splitted2 = splitted[i].Split(new char[] { '\r', '\n' });
+
+                        //    foreach (var s in splitted2)
+                        //    {
+                        //        if (s.Length <= 1)
+                        //        {
+                        //            sb.Append("\r\n");
+                        //            continue;
+                        //        }
+                        //        if (endChars.Contains(s[s.Length - 1]))
+                        //        {
+                        //            sb.Append(s);
+                        //            sb.Append("\r\n");
+                        //        }
+                        //        else
+                        //        {
+                        //            sb.Append(s);
+                        //            sb.Append(' ');
+                        //            continuedPar++;
+                        //        }
+                        //    }
+                        //}
+                        else
+                        {
+                            sb.Append(splitted[i]);
+                            sb.Append(' ');
+                        }
+                    }
+
+                    if (sb.Length > 60000)
+                    {
+                        endString += sb.ToString();
+                        sb.Clear();
+                    }
+
+                }
+
+                endString += sb.ToString();
+            }
+
+
+            richTextBoxOut.Text = endString;
+
+            if (badCharCount > 0 || continuedPar > 0)
+            {
+                if (badCharCount > 0 && continuedPar == 0)
+                {
+                    MessageBox.Show($"{badCharCount} bad chars was removed!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (badCharCount == 0 && continuedPar > 0)
+                {
+                    MessageBox.Show($"{continuedPar} paragraphs continued!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"{badCharCount} bad chars was removed!\n{continuedPar} paragraphs continued!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void buttonPaste_Click(object sender, EventArgs e)
+        {
+            richTextBoxIn.Text = Clipboard.GetText(TextDataFormat.UnicodeText);
+        }
+
+        private void buttonPCC_Click(object sender, EventArgs e)
+        {
+            richTextBoxIn.Text = Clipboard.GetText(TextDataFormat.UnicodeText);
+            Convert();
+            Clipboard.SetText(richTextBoxOut.Text, TextDataFormat.UnicodeText);
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(richTextBoxOut.Text))
+            {
+                Clipboard.SetText(richTextBoxOut.Text, TextDataFormat.UnicodeText);
+            }
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            Convert();
+        }
+
+        private async void Form1_SizeChanged(object sender, EventArgs e)
+        {
+
+            int tbxWidth = (this.Width - 80) / 2;
+            int tbxHeight = this.Height - 200;
+
+            buttonConvert.Location = new Point(this.Width / 2 - buttonConvert.Width / 2, 20);
+            buttonCopy.Location = new Point(this.Width / 2 - buttonCopy.Width / 2 + 100, 20);
+            buttonPaste.Location = new Point(this.Width / 2 - buttonPaste.Width / 2 - 100, 20);
+
+            buttonPCC.Location = new Point(this.Width - buttonPaste.Width - buttonPaste.Width / 2, 20);
+
+            richTextBoxIn.Location = new Point(20, 150);
+            richTextBoxIn.Size = new Size(tbxWidth, tbxHeight);
+            richTextBoxOut.Location = new Point(20 + tbxWidth + 20, 150);
+            richTextBoxOut.Size = new Size(tbxWidth, tbxHeight);
+
+            await Task.Delay(10);
+        }
+
+        private void checkBoxContPar_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxDot.Enabled = checkBoxComma.Enabled = checkBoxColon.Enabled = checkBoxSemicolon.Enabled = checkBoxContPar.Checked;
+        }
+    }
+}
